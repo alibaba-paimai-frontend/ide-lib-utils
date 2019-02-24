09e7fa99baae670ca7ab83f1cb5decc6c0a55510
bocheng.zbc <bocheng.zbc@alibaba-inc.com>
2019-02-24 13:30:58 +0800

    feat(功能增强): 新增 getValueByPath 方法

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -42,3 +42,29 @@ export function isPlainObject(value: any) {
   var proto = Object.getPrototypeOf(value);
   return proto === Object.prototype || proto === null;
 }
+
+
+/**
+ * get value from object
+ * 
+ * see for detail: 
+ *  // https://gomakethings.com/how-to-get-the-value-of-an-object-from-a-specific-path-with-vanilla-js
+ *  // https://stackoverflow.com/questions/6491463/accessing-nested-javascript-objects-with-string-key
+ * @param o - target object
+ * @param s - path string
+ * @param def - default value
+ */
+export function getValueByPath(o: any, s: string, def?: any) {
+  s = s.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
+  s = s.replace(/^\./, '');           // strip a leading dot
+  var a = s.split('.');
+  for (var i = 0, n = a.length; i < n; ++i) {
+    var k = a[i];
+    if (k in o) {
+      o = o[k];
+    } else {
+      return def;
+    }
+  }
+  return o;
+}
