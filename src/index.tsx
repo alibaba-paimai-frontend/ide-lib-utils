b73ce0c0d8ef6e2d2a5a544262f0d1f2e38de9bc
bocheng.zbc <bocheng.zbc@alibaba-inc.com>
2020-03-27 10:05:24 +0800

    feat: 🎸 功能新增: 新增 mitt 导出

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 export * from 'advance-json-merge';
 export * from 'ts-debounce-throttle';
 export * from 'aa-loader';
+import mitt from 'mitt';
 
 export * from './lib/debug';
 export * from './lib/util';
+
+export { mitt };
 
 // 定义版本号
 declare const __VERSION__: string;
